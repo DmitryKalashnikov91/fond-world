ad92593c1f839a5c7b8de9caf864219aa43bad5d
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2023-01-07 08:08:34 +0300

    add newspapers

--- client/src/app/components/pages/newspapers/Newspapers.jsx
+++ client/src/app/components/pages/newspapers/Newspapers.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Alert from 'react-bootstrap/Alert';
+import parse from 'html-react-parser';
 import styles from './Newspapers.module.scss';
 import { newspapers } from './mockNews/news';
 const Newspapers = () => {
@@ -9,14 +10,16 @@ const Newspapers = () => {
                 <Alert variant='secondary' className={styles.newspapers_card} key={item.id}>
                     <div className={styles.newspapers_card__header}>
                         <strong>{item.header}</strong>
-                        <a href={item.hyperlink}>{item.title}</a>
+                        <a href={item.hyperlink} target='_blank' rel='noreferrer'>
+                            {parse(item.title)}
+                        </a>
                     </div>
 
                     <div className={styles.newspapers_card__content}>
                         <span>{item.content}</span>
                         <button className='btn btn-outline-primary'>
                             <a href={item.linkToItem} target='_blank' rel='noreferrer'>
-                                Подробнее...
+                                Читать статью
                             </a>
                         </button>
                     </div>
