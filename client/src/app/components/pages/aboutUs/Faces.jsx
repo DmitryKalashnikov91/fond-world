51d05c3bb620766c5df4427bcaa89a3797106fd4
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2023-05-24 15:03:18 +0300

    add news and faces

--- client/src/app/components/pages/aboutUs/Faces.jsx
+++ client/src/app/components/pages/aboutUs/Faces.jsx
@@ -1,21 +1,64 @@
+import { nanoid } from 'nanoid';
 import Attention from '../../common/Attention';
 import styles from './Faces.module.scss';
+import parse from 'html-react-parser';
+
+const facesData = [
+    {
+        id: nanoid(8),
+        name: 'Игорь А.Алекс',
+        role: 'Главный продюсер и инициатор фонда &quot;Наш Мир 21 век&quot;',
+        mail: 'info@org-nashmir.tech',
+        imgSrc: 'https://firebasestorage.googleapis.com/v0/b/fow-react.appspot.com/o/trustees%2FAAlex.webp?alt=media&token=166ef9ca-5023-4869-9232-5a8ad205c1d4',
+        alt: 'Игорь А.Алекс',
+    },
+    {
+        id: nanoid(8),
+        name: 'Мурадуллаев Рустам Сабирович',
+        role: 'Соорганизатор и попечитель фонда',
+        mail: 'snic2004@mail.ru',
+        imgSrc: 'https://firebasestorage.googleapis.com/v0/b/fow-react.appspot.com/o/trustees%2FRustam.webp?alt=media&token=5156960b-80bd-49f8-9f25-f80915d46a10',
+        alt: 'Мурадуллаев Р.С.',
+    },
+    {
+        id: nanoid(8),
+        name: 'Сивохин Виктор Михайлович',
+        role: 'Соорганизатор акций и форумов',
+        mail: '',
+        imgSrc: 'https://firebasestorage.googleapis.com/v0/b/fow-react.appspot.com/o/avatars%2FSivohinVM.webp?alt=media&token=d26f5018-1582-408f-9384-c48d1fda0779',
+        alt: 'Сивохин В.М.',
+    },
+
+    {
+        id: nanoid(8),
+        name: 'Калашников Дмитрий Александрович',
+        role: 'Заместитель главного продюсера, руководитель службы IT',
+        mail: 'dmitrynashmir@gmail.com',
+        imgSrc: 'https://firebasestorage.googleapis.com/v0/b/fow-react.appspot.com/o/trustees%2FKalashDM.webp?alt=media&token=fc07fb14-3165-47ba-a5b5-e3d332d3e161',
+        alt: 'Калашников Д.А.',
+    },
+];
 
 const Faces = () => {
     return (
         <div className={styles.Faces}>
             <Attention />
-            <ul>
-                <li>Игорь А. Алекс</li>
-                <li>Дмитрий Певцов</li>
-                <li>Тимофей Аникин</li>
-                <li>Кирилл Игнатов</li>
-                <li>Екатерина Кириенко</li>
-                <li>Дмитрий Калашников</li>
-                <li>Надежда Крекшина</li>
-                <li>Елена Лисина</li>
-                <li>Елена Кичаева</li>
-                <li>Лариса Солнечная</li>
+            <h2 className={styles.Faces_head}>Наша команда</h2>
+            <ul className={styles.Faces_items}>
+                {facesData.map((face) => (
+                    <li key={face.id}>
+                        <div className={styles.Faces_items__block}>
+                            <img src={face.imgSrc} alt={face.alt} width={160} />
+                            <h4 className='pt-5'>{face.name}</h4>
+                            <div className={styles.Faces_items__block___text}>
+                                <span>{parse(face.role)}</span>
+                                <p>
+                                    <a href={`mailto:${face.mail}`}>{face.mail}</a>
+                                </p>
+                            </div>
+                        </div>
+                    </li>
+                ))}
             </ul>
         </div>
     );
