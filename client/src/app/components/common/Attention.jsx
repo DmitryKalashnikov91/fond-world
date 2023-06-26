d1b848f140211043addb279e826c6105d235d267
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2023-06-26 10:44:29 +0300

    add main content

--- client/src/app/components/common/Attention.jsx
+++ client/src/app/components/common/Attention.jsx
@@ -1,6 +1,6 @@
 const Attention = () => {
     return (
-        <span className='text-danger'>
+        <span className='text-danger ms-5'>
             <strong>Внимание! </strong>Эта страница временно не работает, т.к. находится в
             разработке.
         </span>
