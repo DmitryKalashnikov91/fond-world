7085bc942160f3200c0a6c285f134afb5e70d593
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2023-08-18 21:00:21 +0300

    replace payform

--- client/src/app/components/common/form/payform/PayForm.jsx
+++ client/src/app/components/common/form/payform/PayForm.jsx
@@ -1,12 +1,7 @@
-import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
-import Form from 'react-bootstrap/Form';
-import Attention from '../../Attention';
 import styles from './PayForm.module.scss';
-import { useState } from 'react';
 
 const PayForm = () => {
-    const [isAgree, setIsAgree] = useState(false);
+    // const [isAgree, setIsAgree] = useState(false);
     return (
         <div className={styles.PayForm}>
             <div className={styles.PayForm_aboutTG}>
@@ -15,59 +10,19 @@ const PayForm = () => {
                 </a>
                 <span> - наш телеграм канал, вступив в который, вы уже становитесь Меценатом.</span>
                 <p>Внести свой вклад в увековечивание памяти можно пожертвовав любой суммой.</p>
-                <span>
+                <p>
+                    На данный момент, взносы принимаются по реквизитам:
+                    <ul className='fw-bold'>
+                        <li>Адрес: 119421, г.Москва, Ленинский пр-т 111, к. 1, 24Н</li>
+                        <li>ИНН: 9728097957 ОГРН: 123770394140 КПП: 772801001</li>
+                        <li>р/с: 4070 3810 0000 0005 405 ПАО "Промсвязьбанк"</li>
+                        <li>к/с: 30101 810 4 0000 0000555 БИК: 044525555</li>
+                    </ul>
                     Денежные средства пойдут на изготовление и установку скульптурных групп и
                     монументов "Дети Донбасса - Дети России", "Героям России", "Волонтерам России" и
                     "Священнослужителям"
-                </span>
-            </div>
-            <Attention />
-
-            <Form>
-                <Form.Group className='mb-3 text-light' controlId='formBasicEmail'>
-                    <Form.Label>Email адресс</Form.Label>
-                    <Form.Control type='email' placeholder='Enter email' />
-                    <Form.Text className='text-muted'>
-                        Мы никогда не будем делиться вашей электронной почтой с кем-либо еще
-                    </Form.Text>
-                </Form.Group>
-                <Form.Select aria-label='Default select example'>
-                    <option>Выбрать сумму</option>
-                    <option value='1' className='text-danger'>
-                        100
-                    </option>
-                    <option value='2' className='text-warning'>
-                        200
-                    </option>
-                    <option value='3' className='text-primary'>
-                        500
-                    </option>
-                    <option value='4' className='text-success'>
-                        1000
-                    </option>
-                    <option value='5'>Другая сумма...</option>
-                </Form.Select>
-
-                <p className='text-light pt-3'>
-                    <input
-                        type='checkbox'
-                        name='oferta'
-                        id='ofert'
-                        onChange={() => setIsAgree(!isAgree)}
-                    />
-                    Принимаю{' '}
-                    <a
-                        href='https://firebasestorage.googleapis.com/v0/b/fow-react.appspot.com/o/DOCS%2Foferta.pdf?alt=media&token=67431f55-3b22-4d27-9bfe-8afbb182b3fe'
-                        target='_blank'
-                        rel='noopener noreferrer'>
-                        публичную оферту
-                    </a>{' '}
-                    и соглашаюсь с <Link to={'/confidency'}> политикой конфиденциальности</Link>
                 </p>
-                <Button type='submit' className='mt-3' disabled={!isAgree}>
-                    Оплата
-                </Button>
-            </Form>
+            </div>
             <p className='text-light'>
                 Другие варианты сотрудничества и вся информация:{' '}
                 <a href='mailto:info@org-nashmir.tech'>info@org-nashmir.tech</a>
