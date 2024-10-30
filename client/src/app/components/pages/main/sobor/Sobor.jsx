5ae2c48dd21707b1c741a1bbe9aac4eacc76347f
DmitryKalashnikov91 <95515184+DmitryKalashnikov91@users.noreply.github.com>
2024-10-30 10:23:27 +0300

    edd tr

--- client/src/app/components/pages/main/sobor/Sobor.jsx
+++ client/src/app/components/pages/main/sobor/Sobor.jsx
@@ -1,5 +1,5 @@
+import { FaCloudDownloadAlt } from 'react-icons/fa'
 import styles from './Sobor.module.scss'
-
 const Sobor = () => {
 	return (
 		<>
@@ -14,6 +14,16 @@ const Sobor = () => {
 						V-й МЕЖДУНАРОДНЫЙ ФОРУМ КУЛЬТУРНО-ЭКОНОМИЧЕСКОГО СОТРУДНИЧЕСТВА "НАШ
 						МИР-XXI ВЕК"
 					</h3>
+					<div className='text-center mb-4	'>
+						<a
+							target='_blank'
+							rel='noreferrer'
+							href='https://firebasestorage.googleapis.com/v0/b/fow-react.appspot.com/o/DOCS%2F%D0%9F%D1%80%D0%B5%D1%81%D1%81-%D1%80%D0%B5%D0%B7%D0%B8%D0%B7-5-%D0%B3%D0%BE-%D0%9C%D0%A4%D0%9A%D0%AD%D0%A1-%D0%9D%D0%B0%D1%88-%D0%9C%D0%B8%D1%80-21-%D0%B2%D0%B5%D0%BA_%D0%9F%D1%80%D0%B5%D0%B7%D0%B8%D0%B4%D0%B5%D0%BD%D1%82_%D0%BE%D1%82%D0%B5%D1%8C-NEW.pdf?alt=media&token=ca347a1d-6f0d-4935-a202-07ecaabc2221'
+						>
+							Пресс-релиз.pdf
+							<FaCloudDownloadAlt className='ms-2' />
+						</a>
+					</div>
 					<ul className='text-light fs-4'>
 						<li>
 							11. 00. Сбор гостей, осмотр выставки, фотозона, пресс-подходы и
